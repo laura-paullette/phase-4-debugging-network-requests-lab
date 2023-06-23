364d03d69a3d7b344a4239b70aa822b9b18bf2dd
laura-paullette <laura.paullette@student.moringaschool.com>
2023-06-23 11:20:13 +0300

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,19 +7,24 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+    toy = Toy.create(toy_params)
+    render json: toy,  status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
+  rescue ApplicationRecord::RecordNotFound
+    render_not_found_response
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
     toy.destroy
     head :no_content
+  rescue ApplicationRecord::RecordNotFound
+    render_not_found_response
   end
 
   private
